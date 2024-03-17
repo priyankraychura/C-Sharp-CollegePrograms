a3936ef9bc07b0834a0b7e19d794bc87f0e654b9
priyankraychura <priyankrkaychura@gmail.com>
2024-03-17 16:58:08 +0530

    Filed tracked

---
+++ PR31_MessageBox_ShowMethod/Form1.cs
@@ -0,0 +1,10 @@
+namespace PR31_MessageBox_ShowMethod
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PR31_MessageBox_ShowMethod/Program.cs
@@ -0,0 +1,17 @@
+namespace PR31_MessageBox_ShowMethod
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
