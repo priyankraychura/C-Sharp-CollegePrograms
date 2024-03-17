8556d64f30e298c6891573a7f8b37172a80a3fc4
priyankraychura <priyankrkaychura@gmail.com>
2024-03-17 16:37:13 +0530

    Images location changes in PictureBox example

--- PR48_PictureBox/Form1.Designer.cs
+++ PR48_PictureBox/Form1.Designer.cs
@@ -44,11 +44,11 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(366, 56);
+            label1.Location = new Point(337, 59);
             label1.Name = "label1";
-            label1.Size = new Size(83, 20);
+            label1.Size = new Size(144, 20);
             label1.TabIndex = 1;
-            label1.Text = "Picture Box";
+            label1.Text = "Picture Box Example";
             // 
             // Form1
             // 
--- PR48_PictureBox/Form1.cs
+++ PR48_PictureBox/Form1.cs
@@ -9,7 +9,7 @@ namespace PR48_PictureBox
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            pictureBox1.Image = Image.FromFile("E://BCA Sem IV//book1.png");
+            pictureBox1.Image = Image.FromFile("E://BCA Sem IV//C#//book1.png");
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
         }
     }
